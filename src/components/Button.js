623e7bd9e7fbd85aa482145efb25f8848f9ab274
diogenesoftoronto <diogenesoft@protonmail.com>
2022-01-18 23:46:37 -0500

    button test all pass

--- src/components/Button.js
+++ src/components/Button.js
@@ -3,9 +3,12 @@ import React from "react";
 import "components/Button.scss";
 export default function Button(props) {
    let buttonClass = "button";
+// ternary chaining
    buttonClass = props.confirm ? buttonClass+= " button--confirm" 
+   : props.danger ? buttonClass += " button--danger" 
    : buttonClass;
-   return <button className={buttonClass}>{props.children}</button>;
+
+   return <button onClick={props.onClick}  className={buttonClass} disabled={props.disabled}>{props.children}</button>;
 }
 
  
