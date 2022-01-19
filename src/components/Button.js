2b78800cea58038f33078c30f8a451130476563f
diogenesoftoronto <diogenesoft@protonmail.com>
2022-01-18 23:59:27 -0500

    classnames function addition, tests pass

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,13 +1,11 @@
 import React from "react";
+import classNames from "classnames";
+
 
 import "components/Button.scss";
 export default function Button(props) {
-   let buttonClass = "button";
-// ternary chaining
-   buttonClass = props.confirm ? buttonClass+= " button--confirm" 
-   : props.danger ? buttonClass += " button--danger" 
-   : buttonClass;
-
+   let buttonClass = classNames("button", {"button--danger": props.danger, "button--confirm": props.confirm});
+   
    return <button onClick={props.onClick}  className={buttonClass} disabled={props.disabled}>{props.children}</button>;
 }
 
