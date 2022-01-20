abe75ad4f06d5cf5eab37680f49c28277dc78294
diogenesoftoronto <diogenesoft@protonmail.com>
2022-01-19 23:36:36 -0500

    daylist functional in storybook

--- src/components/DayList.js
+++ src/components/DayList.js
@@ -2,7 +2,12 @@ import React from 'react';
 import DayListItem from './DayListItem'
 
 const DayList = (props) => {
-  const DayMap = props.days.map((day) => <DayListItem name={day.name} spots={day.spots} setDay={props.setDay} />);
+    const DayMap = props.days.map(
+      day => 
+      day.name === props.day ? 
+      <DayListItem name={day.name} spots={day.spots} setDay={props.setDay} selected />
+    : <DayListItem name={day.name} spots={day.spots} setDay={props.setDay} />);
+
     return (<ul>{DayMap}</ul>);
 }
 
