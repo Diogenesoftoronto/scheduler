99c58b35e79067732e844c64f27683e629fd1ab2
diogenesoftoronto <diogenesoft@protonmail.com>
2022-01-25 01:23:20 -0500

    destructuring of props for a cleaner function

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -3,17 +3,17 @@ import classNames from "classnames";
 import './DayListItem.scss'
 
 export default function DayListItem(props) {
-  // const {name, spots, setDay, selected} = props;
-  const spotsFull = props.spots === 0;
+  const {name, spots, setDay, selected} = props;
+  const spotsFull = spots === 0;
   let formatSpots; 
   formatSpots = spotsFull ? formatSpots = "no spots" 
-  : props.spots > 1 ? formatSpots = props.spots+" spots" 
-  : formatSpots = props.spots+" spot";
+  : spots > 1 ? formatSpots = spots+" spots" 
+  : formatSpots = spots+" spot";
    
-  const liClass = classNames("day-list__item",{"day-list__item--selected": props.selected, "day-list__item--full": spotsFull});
+  const liClass = classNames("day-list__item",{"day-list__item--selected": selected, "day-list__item--full": spotsFull});
   return (
-    <li className={liClass} onClick={() => props.setDay(props.name)}>
-      <h2 className="text--regular">{props.name}</h2> 
+    <li className={liClass} onClick={() => setDay(props.name)}>
+      <h2 className="text--regular">{name}</h2> 
       <h3 className="text--light">{formatSpots} remaining</h3>
     </li>
   );
