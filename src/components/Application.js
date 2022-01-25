2582b8d63e6eac87c6a1fc1ef07a1d4bd71f2c2c
diogenesoftoronto <diogenesoft@protonmail.com>
2022-01-25 01:22:21 -0500

    import hook and remove hardcoded values

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,73 +1,21 @@
 
-import React, {useState} from "react";
-import DayList from "components/DayList"
-import Appointment from "components/Appointment";
-import "components/Application.scss";
+import React, {useState, useEffect} from "react";
+import DayList from "./DayList"
+import Appointment from "./Appointment";
+import "./Application.scss";
+import useDays from "./hooks/useDays";
 
 
-const days = [
-  {
-    id: 1,
-    name: "Monday",
-    spots: 2,
-  },
-  {
-    id: 2,
-    name: "Tuesday",
-    spots: 5,
-  },
-  {
-    id: 3,
-    name: "Wednesday",
-    spots: 0,
-  },
-];
-
-const appointments = [
-  {
-    id: 1,
-    time: "12pm",
-  },
-  {
-    id: 2,
-    time: "1pm",
-    interview: {
-      student: "Lydia Miller-Jones",
-      interviewer:{
-        id: 3,
-        name: "Sylvia Palmer",
-        avatar: "https://i.imgur.com/LpaY82x.png",
-      }
-    }
-  },
-  {
-    id: 3,
-    time: "2pm",
-  },
-  {
-    id: 4,
-    time: "3pm",
-    interview: {
-      student: "Archie Andrews",
-      interviewer:{
-        id: 4,
-        name: "Cohana Roy",
-        avatar: "https://i.imgur.com/FK8V841.jpg",
-      }
-    }
-  },
-  {
-    id: 5,
-    time: "4pm",
-  }
-];
 
 
-const AppointmentMap = appointments.map(appointment => <Appointment  {...appointment} />)
+
 
 
 export default function Application(props) {
- const [day, setDay] = useState("Monday");
+  const [day, setDay] = useState("Monday");
+  const [appointments, setAppointments] = useState([]);
+  const [interviewers, setInterviewers] = useState([]);
+  const [days, setDays] = useDays('http://localhost:8001/api/days');
   return (
     <main className="layout">
       <section className="sidebar">
