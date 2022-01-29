e97d1012cf74ce85cb9fa1c427b0a86fafa98a10
diogenesoftoronto <diogenesoft@protonmail.com>
2022-01-28 21:18:27 -0500

    add the create component and back transitions functionality

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -11,20 +11,23 @@ const SHOW = 'SHOW';
 const CREATE = 'CREATE';
 
 const Appointment = props => {
-  const { mode, transition, back } = useVisualMode(
-    props.interview ? SHOW : EMPTY
-    );
   const {id, time, interview, onEdit, onDelete, onAdd} = props;
+  const { mode, transition, back } = useVisualMode(
+    interview ? SHOW : EMPTY
+    );
   const onSave = (name, interviewer) => {console.log(name, interviewer)}
   let showComponent = false;
   let createComponent = false;
   if (mode === SHOW) {
     const {student, interviewer} = interview;
+    console.log(interview)
     showComponent = <Show student={student} interviewer={interviewer} onEdit={onEdit} onDelete={onDelete}/>
   }
   console.log(mode)
   if (mode === CREATE) {
-    createComponent = <Form interviewers={[]} interview={interview} onCancel={back} onSave={onSave} student={interview.student}/>
+    console.log("in create mode", interview)
+    createComponent = !interview ? <Form interviewers={[]} interview={{student: "N/A", interviewer:{ id: "N/A", name: "N/A", avatar: 'https://image.shutterstock.com/image-illustration/not-available-red-rubber-stamp-260nw-586791809.jpg'}}} onCancel={back} onSave={onSave} student={"N/A"}/> 
+    : <Form interviewers={[]} interview={interview} onCancel={back} onSave={onSave} student={interview.student}/>
   }
   let appointStr;
   // "Appointment at "+time
@@ -33,7 +36,7 @@ const Appointment = props => {
   
   return <article className="appointment"><Header time={time}/>
     {showComponent ? showComponent 
-    : createComponent ? createComponent : <Empty onAdd={onAdd}/>}
+    : createComponent ? createComponent : <Empty onAdd={() => transition(CREATE)}/>}
       {appointStr}
     </article>
 };
