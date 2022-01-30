2d9a76dbcf969679a73603bf53fd947ca071348b
diogenesoftoronto <diogenesoft@protonmail.com>
2022-01-29 22:29:00 -0500

    saving and deleting appointments now possible through added functions save and onDelet state is sent to the server and preserved

--- src/components/hooks/useApplication.js
+++ src/components/hooks/useApplication.js
@@ -1,54 +1,100 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-
-const useDays = (urls, day) => {
-   const [data, setData] = useState({
-  day,
-  days: [],
-  appointments: {},
-  interviewers: {},
-  pending: true,
-  error: null
+const useApplication = (
+  urls = ["/api/days", "/api/appointments", "/api/interviewers"],
+  day = "Monday"
+) => {
+  const [state, setState] = useState({
+    day,
+    days: [],
+    appointments: {},
+    interviewers: {},
+    pending: true,
+    error: null,
   });
-  // console.log("hello world", urls);
-  // return the responses from the axios requests and set the data state
-  function getData (urls) {
-    const getResponses = urls.map(url => axios.get(url))
-    Promise.all(getResponses).then(responseArr => {
-      // console.log("responseArr", responseArr);
-      const [daysResponse, appointmentsResponse, interviewersResponse] = responseArr;
-    
-      // console.log("daysResponse", daysResponse.data)
-        // console.log("appointmentsResponse", appointmentsResponse.data)
-          // console.log("interviewersResponse", interviewersResponse.data)
-        setData(prev => {
-          return ({
-          ...prev,
-          // days: daysResult,
-          // appointments: appointmentsResult,
-          // interviewers: interviewersResult,
-          days: daysResponse.data,
-          appointments: appointmentsResponse.data,
-          interviewers: interviewersResponse.data,
-          pending: false,
-          error: null
-        })
+  const setDay = (day) => setState({ ...state, day });
+  // This function will change the local state when we book an interview.
+  const bookInterview = async (id, interview) => {
+    const appointment = {
+      ...state.appointments[id],
+      interview: { ...interview },
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+
+    // sends a put request using axios to the server with the useEffect hook
+    const bookingPromise = axios
+      .put(`/api/appointments/${id}`, {
+        interview: { ...interview },
       })
-    }
-      ).catch(error => {
-        setData(prev =>( {
+      .then(setState({ ...state, appointments }));
+
+    // console.log("bookInterview", id, interview, appointments, appointments); // id, interview
+    return bookingPromise;
+  };
+
+  const deleteInterview = async (id) => {
+    const appointment = {
+      ...state.appointments[id],
+      interview: null,
+    };
+    const appointments = {
+      ...state.appointments,
+      [id]: appointment,
+    };
+    const deleteInterviewPromise = axios
+      .delete(`/api/appointments/${id}`)
+      .then(setState({ ...state, appointments }));
+    // console.log("cancelInterview", id, appointment, appointments);
+    return deleteInterviewPromise;
+  };
+  // console.log("hello world", urls);
+  // return the responses from the axios requests and set the state state
+
+  /**
+   *@param {String[]} urls
+   */
+  function getstate(urls) {
+    const getResponses = urls.map((url) => axios.get(url));
+    Promise.all(getResponses)
+      .then((responseArr) => {
+        // console.log("responseArr", responseArr);
+        const [daysResponse, appointmentsResponse, interviewersResponse] =
+          responseArr;
+
+        // console.log("daysResponse", daysResponse.data);
+        // console.log("appointmentsResponse", appointmentsResponse.data);
+        // console.log("interviewersResponse", interviewersResponse.data);
+        setState((prev) => {
+          return {
+            ...prev,
+            // days: daysResult,
+            // appointments: appointmentsResult,
+            // interviewers: interviewersResult,
+            days: daysResponse.data,
+            appointments: appointmentsResponse.data,
+            interviewers: interviewersResponse.data,
+            pending: false,
+            error: null,
+          };
+        });
+      })
+      .catch((error) => {
+        setState((prev) => ({
           ...prev,
           pending: false,
-          error: error
+          error: error,
         }));
       });
   }
-  // useEffect to get the data
-  useEffect(() => getData(urls), [urls]);
-  
+  // useEffect to get the state
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  useEffect(() => getstate(urls), [...urls]);
 
-  return [data, setData];
-}
+  return { state, setState, setDay, bookInterview, deleteInterview };
+};
 
-export default useDays;
+export default useApplication;
