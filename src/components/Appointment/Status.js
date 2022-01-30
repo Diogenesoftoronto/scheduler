aeab7448da5142d276c58b5e4cd735ecfbb80232
diogenesoftoronto <diogenesoft@protonmail.com>
2022-01-29 22:34:12 -0500

    linting and additional comments

--- src/components/Appointment/Status.js
+++ src/components/Appointment/Status.js
@@ -1,16 +1,17 @@
-import React from 'react';
+import React from "react";
 
 const Status = (props) => {
-  const {message} = props;
-  return <main className="appointment__card appointment__card--status">
-  <img
-    className="appointment__status-image"
-    src="images/status.png"
-    alt="Loading"
-  />
-  <h1 className="text--semi-bold">{message}</h1>
-</main>
-;
+  const { message } = props;
+  return (
+    <main className="appointment__card appointment__card--status">
+      <img
+        className="appointment__status-image"
+        src="images/status.png"
+        alt="Loading"
+      />
+      <h1 className="text--semi-bold">{message}</h1>
+    </main>
+  );
 };
 
 export default Status;
