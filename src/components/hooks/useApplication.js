7c18ebad5ba354d37024b86465e19a4c0a585527
diogenesoftoronto <diogenesoft@protonmail.com>
2022-02-01 05:15:32 -0500

    add update spots functionality to the application and remove asynchronous functionality from delete and book interview function. all tests pass, spots update successfully, however legacy function deos not work giveing duplication errors on the first element of the arry set to fix then switch over at a later date

--- src/components/hooks/useApplication.js
+++ src/components/hooks/useApplication.js
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
+import { getAppointmentsForDay } from "../../helpers/selectors";
 
 const useApplication = (
   urls = ["/api/days", "/api/appointments", "/api/interviewers"],
@@ -15,7 +16,7 @@ const useApplication = (
   });
   const setDay = (day) => setState({ ...state, day });
   // This function will change the local state when we book an interview.
-  const bookInterview = async (id, interview) => {
+  const bookInterview = (id, interview) => {
     const appointment = {
       ...state.appointments[id],
       interview: { ...interview },
@@ -24,19 +25,24 @@ const useApplication = (
       ...state.appointments,
       [id]: appointment,
     };
-
+    const days = updateSpots(appointments);
+    console.log("yo", state);
     // sends a put request using axios to the server with the useEffect hook
     const bookingPromise = axios
       .put(`/api/appointments/${id}`, {
         interview: { ...interview },
       })
-      .then(setState({ ...state, appointments }));
+      .then(() => {
+        setState({ ...state, days, appointments });
+      });
 
     // console.log("bookInterview", id, interview, appointments, appointments); // id, interview
     return bookingPromise;
   };
+  // This function will update the spots when we book or cancel an interview via the useEffect hook, and setState.
 
-  const deleteInterview = async (id) => {
+  // This function will update the spots when we book or delete an interview via setState.
+  const deleteInterview = (id) => {
     const appointment = {
       ...state.appointments[id],
       interview: null,
@@ -45,12 +51,70 @@ const useApplication = (
       ...state.appointments,
       [id]: appointment,
     };
+    const days = updateSpots(appointments);
     const deleteInterviewPromise = axios
       .delete(`/api/appointments/${id}`)
-      .then(setState({ ...state, appointments }));
+      .then(() => {
+        setState({ ...state, days, appointments });
+      });
+
     // console.log("cancelInterview", id, appointment, appointments);
     return deleteInterviewPromise;
   };
+  const updateSpots = (appointments) => {
+    // loop over days
+    const nullSpotsForDay = (day, appointments) => {
+      let counter = 0;
+      for (const id of day.appointments) {
+        if (appointments[id].interview === null) {
+          counter+=1;
+        }
+
+      }
+      return counter;
+    }
+    const updateDays = state.days.map((currentDay) => {
+
+      return {
+        ...currentDay, 
+        spots: nullSpotsForDay(currentDay, appointments)
+      }
+    })
+    return updateDays;
+  }
+
+  // find appointments where the interview is null  and set the spots to the number of those appointments;
+  const updateSpots2 = () => {
+    const appointmentsForDay = getAppointmentsForDay(state, state.day);
+    const appWhereInterviewNull = appointmentsForDay.filter((appointment) => {
+      return appointment.interview === null;
+    });
+    const spots = appWhereInterviewNull.length;
+    const findDayByName = state.days.findIndex((day) => day.name = state.day)
+
+    const day = {
+      ...state.days[findDayByName],
+      spots
+    };
+    const days = [
+      ...state.days
+    ];
+    days[findDayByName] = day
+
+    console.log("dayyyyyy",day);
+    // days of the week
+ 
+   
+
+    // console.log("updateSpots", day, id, state.appointments, spots);
+    // sends a put request using axios to the server
+    // const spotsPromise = axios
+    // .put(`/api/days/${id}`, { spots })
+    // .then(setState({ ...state, days }));
+    console.log("in update", days);
+    return days;
+  };
+
   // console.log("hello world", urls);
   // return the responses from the axios requests and set the state state
 
@@ -94,7 +158,13 @@ const useApplication = (
   // eslint-disable-next-line react-hooks/exhaustive-deps
   useEffect(() => getstate(urls), [...urls]);
 
-  return { state, setState, setDay, bookInterview, deleteInterview };
+  return {
+    state,
+    setState,
+    setDay,
+    bookInterview,
+    deleteInterview,
+  };
 };
 
 export default useApplication;
