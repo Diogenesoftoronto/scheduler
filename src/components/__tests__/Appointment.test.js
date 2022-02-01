8c9d4268a06841cf7a8fff77fe8518047a697b80
diogenesoftoronto <diogenesoft@protonmail.com>
2022-02-01 05:09:34 -0500

    add tests for form and appointments final jest test for form fails. could not git confirm on save to function

---
+++ src/components/__tests__/Appointment.test.js
@@ -0,0 +1,30 @@
+import React from "react";
+
+import { render, cleanup } from "@testing-library/react";
+
+import Appiontment from "../Appointment";
+
+afterEach(cleanup);
+
+it("renders without crashing", () => {
+  render(<Appiontment />);
+});
+it("doesn't call the function", () => {
+  const fn = jest.fn();
+  expect(fn).toHaveBeenCalledTimes(0);
+});
+it("calls the function", () => {
+  const fn = jest.fn();
+  fn();
+  expect(fn).toHaveBeenCalledTimes(1);
+ });
+ it("calls the function with specific arguments", () => {
+  const fn = jest.fn();
+  fn(10);
+  expect(fn).toHaveBeenCalledWith(10);
+ });
+ it("uses the mock implementation", () => {
+  const fn = jest.fn((a, b) => 42);
+  fn(1, 2);
+  expect(fn).toHaveReturnedWith(42);
+ });
